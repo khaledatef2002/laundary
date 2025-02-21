54449ea91eecd31935a2a09062c2eb79c7129a9b
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2025-02-21 15:37:03 +0200

    Update dashboard.php

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -31,7 +31,7 @@ Route::name('dashboard.')->prefix(LaravelLocalization::setLocale() . '/dashboard
         Route::get('/invoice/{invoice}/download', function(Invoice $invoice){
             return Pdf::view('dashboard.templates.invoice', compact('invoice'))
                     ->withBrowsershot(function(Browsershot $browsershot){
-                        $browsershot->setNodeBinary('/root/.nvm/versions/node/v22.14.0/bin/node')
+                        $browsershot->setNodeBinary('/opt/nodejs/bin/node')
                         ->setNpmBinary('/root/.nvm/versions/node/v16.20.2/bin/npm');
                     })
                     ->format(Format::A5)
